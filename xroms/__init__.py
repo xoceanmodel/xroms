856fe0d43302aded429abf00c5792b406e2a01d8
hetland <hetland@tamu.edu>
2020-05-29 16:31:40 -0500

    added hgrad to exposed functions

--- xroms/__init__.py
+++ xroms/__init__.py
@@ -1,5 +1,6 @@
 from .xroms import (roms_dataset,
                     open_roms_netcdf_dataset,
-                    open_roms_zarr_dataset)
+                    open_roms_zarr_dataset,
+                    hgrad)
 from .roms_seawater import density, buoyancy
 from .utilities import to_rho, to_psi, xisoslice
