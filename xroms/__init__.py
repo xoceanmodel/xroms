c29b8abfbeee45992e1165c16fb8b2819c88378e
hetland <hetland@tamu.edu>
2020-05-29 16:17:48 -0500

    modified init to import functions in roms_seawater and utilities

--- xroms/__init__.py
+++ xroms/__init__.py
@@ -1,3 +1,5 @@
 from .xroms import (roms_dataset,
                     open_roms_netcdf_dataset,
                     open_roms_zarr_dataset)
+from .roms_seawater import density, buoyancy
+from .utilities import to_rho, to_psi, xisoslice
