a2d3dd0cc2b8d35250fbd9c995548d483f956abf
Filipe Fernandes <ocefpaf@gmail.com>
2020-06-04 13:11:50 -0300

    add packaging and testing stuff

---
+++ tests/test_dummy.py
@@ -0,0 +1,5 @@
+# To be replaced with real tests soon
+
+def test_imports():
+    import xroms
+    import xroms.roms_seawater
--- xroms/__init__.py
+++ xroms/__init__.py
@@ -1,3 +1,9 @@
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
+
+
 from .xroms import (roms_dataset,
                     open_roms_netcdf_dataset,
                     open_roms_zarr_dataset,
