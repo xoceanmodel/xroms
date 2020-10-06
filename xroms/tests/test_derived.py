2e324d0de35e9c78f06752d8260f2510cbe5004d
Kristen Thyng <kthyng@gmail.com>
2020-10-06 12:17:14 -0500

    reorganized and updated tests

---
+++ xroms/tests/test_derived.py
@@ -0,0 +1,132 @@
+"""Test derived functions."""
+
+import cartopy
+import numpy as np
+import xarray as xr
+
+from xgcm import grid as xgrid
+
+import xroms
+
+
+grid1 = xr.open_dataset("xroms/tests/input/grid.nc")
+# ds = xroms.open_netcdf('xroms/tests/input/ocean_his_0001.nc')
+ds = xr.open_dataset("xroms/tests/input/ocean_his_0001.nc")
+# combine the two:
+ds = ds.merge(grid1, overwrite_vars=True, compat="override")
+ds, grid = xroms.roms_dataset(ds)
+# missing psi grid in variables
+ds = ds.assign_coords({'lon_psi': ds.lon_psi, 'lat_psi': ds.lat_psi})
+
+# functions in test files:
+xl, yl, N = 14, 9, 3
+z_rho = np.array([-97.5025, -50.05, -2.5975])  # eta_rho=0, xi_rho=0
+dx = 14168.78734979  # when eta_rho=0
+dy = 18467.47219268  # when eta_rho=0
+u = np.linspace(0, 1.2, xl - 1)
+v = np.linspace(-0.1, 0.1, yl - 1)[:, np.newaxis]
+temp = np.linspace(15, 20, N)
+salt = np.linspace(25, 15, N)
+zeta = np.linspace(-0.1, 0.1, xl)
+g = 9.81
+rho0 = 1025
+
+
+
+
+
+def test_speed():
+    
+    # run speed in xroms
+    xvar = xroms.speed(ds.u, ds.v, grid)
+    
+    # calculate own version of speed
+    var = np.sqrt(u ** 2 + v ** 2)
+    
+    # these aren't identical because of grid shifting
+    assert np.allclose(
+        xvar.mean(), var.mean(), rtol=1e-2
+    )
+
+
+def test_KE():
+    rho = xroms.density(temp, salt, z_rho)[:, np.newaxis, np.newaxis]
+    s2 = (u ** 2 + v ** 2)[np.newaxis, :, :]
+    KE = 0.5 * rho * s2
+    
+    # xroms
+    s = xroms.speed(ds.u, ds.v, grid)
+    assert np.allclose(
+        xroms.to_grid(xroms.KE(ds.rho0, s), grid, hcoord="psi").mean(), KE.mean(), rtol=1e-2
+    )
+
+    
+def test_uv_geostrophic():
+    vg = 0  # zeta only varies in xi direction
+    # test one corner of domain
+    f = ds.f[0,0].values
+    # correct dx value (eta=0)
+    dzetadxi = (zeta[2] - zeta[0]) / (2 * dx)
+    ug = -g * dzetadxi / f
+    
+    assert np.allclose(xroms.uv_geostrophic(ds.zeta, ds.f, grid, which='xi')[0,0,0], ug)
+    assert np.allclose(xroms.uv_geostrophic(ds.zeta, ds.f, grid, which='eta'), vg)
+
+    
+def test_EKE():
+    vg = 0  # zeta only varies in xi direction
+    # test one corner of domain
+    f = ds.f[0,0].values
+    # correct dx value (eta=0)
+    dzetadxi = (zeta[2] - zeta[0]) / (2 * dx)
+    ug = -g * dzetadxi / f
+    EKE = 0.5*(ug**2 + vg**2)
+    
+    xug, xvg = xroms.uv_geostrophic(ds.zeta, ds.f, grid, which='both')    
+    
+    assert np.allclose(xroms.EKE(xug, xvg, grid)[0,0,0], EKE)
+
+
+def test_dudz():
+    assert np.allclose(xroms.dudz(ds.u, grid), 0)
+
+
+def test_dvdz():
+    assert np.allclose(xroms.dvdz(ds.v, grid), 0)
+    
+    
+def test_vertical_shear():
+    xdudz = xroms.dudz(ds.u, grid)
+    xdvdz = xroms.dvdz(ds.v, grid)
+    assert np.allclose(xroms.vertical_shear(xdudz, xdvdz, grid), 0)
+
+    
+def test_relative_vorticity():
+    assert np.allclose(xroms.relative_vorticity(ds.u, ds.v, grid), 0)
+
+
+def test_ertel():
+    v_z = 0
+    u_z = 0
+    # test one corner of domain
+    f = ds.f[0,0].values
+    vort = 0
+    sig0 = xroms.density(temp, salt, 0)
+    buoy = -g * sig0 / rho0
+    phi_z = (buoy[2] - buoy[0]) / (z_rho[2] - z_rho[0])
+    ertel = -v_z + u_z + (f + vort) * phi_z
+    
+    xsig0 = xroms.potential_density(ds.temp, ds.salt)
+    xbuoy = xroms.buoyancy(xsig0)
+
+    assert np.allclose(xroms.ertel(xbuoy, ds.u, ds.v, ds.f, grid)[0, 1, 0, 0], ertel)
+    
+
+def test_w():
+    # VRX
+    pass
+    
+    
+def test_omega():
+    # VRX
+    pass
---
+++ xroms/tests/test_roms_seawater.py
@@ -0,0 +1,83 @@
+"""Test roms_seawater functions."""
+
+import cartopy
+import numpy as np
+import xarray as xr
+
+from xgcm import grid as xgrid
+
+import xroms
+
+
+grid1 = xr.open_dataset("xroms/tests/input/grid.nc")
+# ds = xroms.open_netcdf('xroms/tests/input/ocean_his_0001.nc')
+ds = xr.open_dataset("xroms/tests/input/ocean_his_0001.nc")
+# combine the two:
+ds = ds.merge(grid1, overwrite_vars=True, compat="override")
+ds, grid = xroms.roms_dataset(ds)
+# missing psi grid in variables
+ds = ds.assign_coords({'lon_psi': ds.lon_psi, 'lat_psi': ds.lat_psi})
+
+# functions in test files:
+xl, yl, N = 14, 9, 3
+z_rho = np.array([-97.5025, -50.05, -2.5975])  # eta_rho=0, xi_rho=0
+dx = 14168.78734979  # when eta_rho=0
+dy = 18467.47219268  # when eta_rho=0
+u = np.linspace(0, 1.2, xl - 1)
+v = np.linspace(-0.1, 0.1, yl - 1)[:, np.newaxis]
+temp = np.linspace(15, 20, N)
+salt = np.linspace(25, 15, N)
+zeta = np.linspace(-0.1, 0.1, xl)
+g = 9.81
+rho0 = 1025
+
+
+
+def test_rho():
+    rho = xroms.density(temp, salt, z_rho)
+    xrho = xroms.density(ds.temp, ds.salt, ds.z_rho)
+    assert np.allclose(xrho[0, :, 0, 0], rho)
+
+
+def test_potential_density():
+    sig0 = xroms.density(temp, salt, 0)
+    xsig0 = xroms.potential_density(ds.temp, ds.salt)
+    assert np.allclose(xsig0[0, :, 0, 0], sig0)
+
+    
+def test_buoyancy():
+    xsig0 = xroms.potential_density(ds.temp, ds.salt)
+    xbuoy = xroms.buoyancy(xsig0)
+    sig0 = xroms.density(temp, salt, 0)
+    buoy = -g * sig0 / rho0
+    assert np.allclose(xbuoy[0, :, 0, 0], buoy)
+
+    
+def test_N2():
+    rho = xroms.density(temp, salt, z_rho)
+    drhodz = (rho[2] - rho[0]) / (z_rho[2] - z_rho[0])
+    var = -g * drhodz / rho0
+    xrho = xroms.density(ds.temp, ds.salt, ds.z_rho)
+    # compare above estimate with two averaged since
+    # there is a grid change
+    assert np.allclose(xroms.N2(xrho, grid)[0, 1:3, 0, 0].mean(), var)
+
+
+# def test_M2():
+#     z_rho_xi2 = np.array([-97.50173077, -50.03461538,  -2.5675])
+# #     z_rho_xi1 = np.array([-97.50211538, -50.04230769, -2.5825])
+#     rho_xi0 = xroms.density(temp, salt, z_rho)
+# #     rho_xi1 = xroms.density(temp, salt, z_rho_xi1)
+#     rho_xi2 = xroms.density(temp, salt, z_rho_xi2)
+#     drhodxi = (rho_xi2[1] - rho_xi0[1]) / (2 * dx)
+#     drhodeta = 0
+#     var = np.sqrt(drhodxi ** 2 + drhodeta ** 2) * g / rho0
+#     xrho = xroms.density(ds.temp, ds.salt, ds.z_rho)
+#     assert np.allclose(xroms.M2(xrho, grid), var)
+
+
+def test_mld():
+    # choose threshold so that z_rho[-2] is the mld
+    xsig0 = xroms.density(ds.temp, ds.salt, 0)
+    thresh = xsig0[0,-2,0,0] - xsig0[0,-1,0,0]
+    assert np.allclose(xroms.mld(xsig0, ds.h, ds.mask_rho, thresh=thresh)[0, 0, 0], z_rho[-2])
